0dfab6e6dc00249efad15301ca81e826745f534f
DivLoic <ldivad@isep.fr>
2020-02-15 01:03:19 +0100

    prepare the gcp and ccloud env

--- kube-secrets.sh
+++ kube-secrets.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
+
+[[ -z "${ENV}" ]] && { echo "ENV was empty"; exit 1; }
+[[ -z "${API_KEY}" ]] && { echo "API_KEY was empty"; exit 1; }
+[[ -z "${SASL_CLASS}" ]] && { echo "SASL_CLASS was empty"; exit 1; }
+[[ -z "${SR_API_KEY}" ]] && { echo "SR_API_KEY was empty"; exit 1; }
+[[ -z "${SECRET_KEY}" ]] && { echo "SECRET_KEY was empty"; exit 1; }
+[[ -z "${SR_SECRET_KEY}" ]] && { echo "SR_SECRET_KEY was empty"; exit 1; }
+[[ -z "${BOOTSTRAP_SERVERS}" ]] && { echo "BOOTSTRAP_SERVERS was empty"; exit 1; }
+[[ -z "${SCHEMA_REGISTRY_URL}" ]] && { echo "SCHEMA_REGISTRY_URL was empty"; exit 1; }
+
 export B64_API_KEY=$(base64 <(echo ${API_KEY}))
-export B64_SECRET_KEY=$(base64 <(echo ${SECRET_KEY}))
-export B64_BOOTSTRAP_SERVERS=$(base64 <(echo ${BOOTSTRAP_SERVERS}))
-export B64_SCHEMA_REGISTRY_URL=$(base64 <(echo ${SCHEMA_REGISTRY_URL}))
 export B64_SASL_CLASS=$(base64 <(echo ${SASL_CLASS}))
 export B64_SR_API_KEY=$(base64 <(echo ${SR_API_KEY}))
+export B64_SECRET_KEY=$(base64 <(echo ${SECRET_KEY}))
 export B64_SR_SECRET_KEY=$(base64 <(echo ${SR_SECRET_KEY}))
+export B64_BOOTSTRAP_SERVERS=$(base64 <(echo ${BOOTSTRAP_SERVERS}))
+export B64_SCHEMA_REGISTRY_URL=$(base64 <(echo ${SCHEMA_REGISTRY_URL}))
 
 envsubst > $(dirname "$0")/.secrets/staging.yaml <<- "EOF"
 ---
