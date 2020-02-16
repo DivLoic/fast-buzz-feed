8d68e70bee7649057e97e390b14a537c82af0795
Elhadi CHERIFI <dadycitroen@gmail.com>
2020-02-16 22:13:27 +0100

    Add aggregator tests

---
+++ fizz-buzz-streams/src/test/java/com/pubsap/eng/BaseCaseTest.java
@@ -0,0 +1,26 @@
+package com.pubsap.eng;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by loicmdivad.
+ */
+public class BaseCaseTest {
+
+    @BeforeAll
+    public static void setUp() {
+    }
+
+    @AfterAll
+    public static void tearDown() {
+    }
+
+    @Test
+    public void functionShouldDoSomething() {
+        fail("Testing the proper execution of unit tests!");
+    }
+}
