10865d2232a2abb92e03149be9437cbb88c4702a
Elhadi CHERIFI <dadycitroen@gmail.com>
2020-02-21 09:52:07 +0100

    Refactoring topic Names + configuration of serdes

--- fizz-buzz-streams/src/test/java/com/pubsap/eng/streams/FizzBuzzAggregatorTest.java
+++ fizz-buzz-streams/src/test/java/com/pubsap/eng/streams/FizzBuzzAggregatorTest.java
@@ -5,7 +5,6 @@ import com.pubsap.eng.schema.*;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
-import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.streams.*;
@@ -14,12 +13,10 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.time.Instant;
 import java.util.*;
 
-import static org.apache.kafka.common.utils.Utils.mkEntry;
-import static org.apache.kafka.common.utils.Utils.mkMap;
+import static com.pubsap.eng.common.FizzUtils.mapFormConfig;
+import static io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -28,10 +25,9 @@ public class FizzBuzzAggregatorTest {
     private static TopologyTestDriver testDriver;
     private static TestInputTopic<InputKey, Item> inputTopic;
     private static TestOutputTopic<OutputKey, Output> outputTopic;
-    final private static String srConfigKey = "schema.registry.url";
 
     @BeforeAll
-    public static void setUp() throws IOException, RestClientException {
+    public static void setUp() {
 
         final Config config = ConfigFactory.load();
 
@@ -41,8 +37,13 @@ public class FizzBuzzAggregatorTest {
         properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
 
-        String inputTopicName = config.getString("aggregator.input.topic.name");
-        String outputTopicName = config.getString("aggregator.output.topic.name");
+        properties.putAll(mapFormConfig(config.getConfig("kafka-client")));
+
+        Map<String, Object> schemaRegistryConfigMap = mapFormConfig(config.getConfig("schema-client"));
+        schemaRegistryConfigMap.put(SCHEMA_REGISTRY_URL_CONFIG, config.getString(SCHEMA_REGISTRY_URL_CONFIG));
+
+        String inputTopicName = "input-topic";
+        String outputTopicName = "output-topic";
 
         MockSchemaRegistryClient mockedRegistry = new MockSchemaRegistryClient();
 
@@ -51,13 +52,11 @@ public class FizzBuzzAggregatorTest {
         SpecificAvroSerde<OutputKey> outputKeySerde = new SpecificAvroSerde<>(mockedRegistry);
         SpecificAvroSerde<Output> outputSerde = new SpecificAvroSerde<>(mockedRegistry);
 
-        itemSerde.configure(Collections.singletonMap(srConfigKey, config.getString(srConfigKey)), false);
-        outputSerde.configure(Collections.singletonMap(srConfigKey, config.getString(srConfigKey)), false);
-        inputKeySerde.configure(Collections.singletonMap(srConfigKey, config.getString(srConfigKey)), true);
-        outputKeySerde.configure(Collections.singletonMap(srConfigKey, config.getString(srConfigKey)), true);
+        itemSerde.configure(schemaRegistryConfigMap, false);
+        outputSerde.configure(schemaRegistryConfigMap, false);
+        inputKeySerde.configure(schemaRegistryConfigMap, true);
+        outputKeySerde.configure(schemaRegistryConfigMap, true);
 
-        mockedRegistry.register(inputTopic + "-value", Item.SCHEMA$);
-        mockedRegistry.register(outputTopic + "-value", Output.SCHEMA$);
 
         TimeWindows timeWindow = TimeWindows
 
@@ -72,7 +71,7 @@ public class FizzBuzzAggregatorTest {
         Produced<OutputKey, Output> producedCounts = Produced.with(outputKeySerde, outputSerde);
 
         StreamsBuilder builder = new StreamsBuilder();
-        builder.stream("input-topic", consumedItems)
+        builder.stream(inputTopicName, consumedItems)
                 .groupByKey(groupedItem)
                 .windowedBy(timeWindow)
                 .aggregate(
@@ -82,7 +81,7 @@ public class FizzBuzzAggregatorTest {
                 )
                 .toStream()
                 .map(FizzBuzzMapper.formatOutput)
-                .to("output-topic",producedCounts);
+                .to(outputTopicName, producedCounts);
 
         testDriver = new TopologyTestDriver(builder.build(), properties);
 
@@ -109,15 +108,15 @@ public class FizzBuzzAggregatorTest {
         inputTopic.pipeKeyValueList(inputValues);
 
         //Then
-        final KeyValue<OutputKey,Output> expectedResult = new KeyValue<>(new OutputKey("client-1", "", ""), new Output(1, 1, 1));
+        final KeyValue<OutputKey, Output> expectedResult = new KeyValue<>(new OutputKey("client-1", "windowStartNotUsed", "windowStopNotUsed"), new Output(1, 1, 1));
 
-        List<KeyValue<OutputKey,Output>> result = outputTopic.readKeyValuesToList();
+        List<KeyValue<OutputKey, Output>> result = outputTopic.readKeyValuesToList();
 
-        final KeyValue<OutputKey,Output> actualResult = result.get(result.size()-1);
+        final KeyValue<OutputKey, Output> actualResult = result.get(result.size() - 1);
 
         assertEquals(expectedResult.value, actualResult.value);
         assertEquals(expectedResult.key.getName(), actualResult.key.getName());
-        assertTrue(result.size()==3);
+        assertTrue(result.size() == 3);
     }
 
     @Test
@@ -137,15 +136,15 @@ public class FizzBuzzAggregatorTest {
         inputTopic.pipeKeyValueList(inputValues);
 
         //Then
-        final KeyValue<OutputKey,Output> expectedResult = new KeyValue<>(new OutputKey("client-2", "", ""), new Output(2, 2, 2));
+        final KeyValue<OutputKey, Output> expectedResult = new KeyValue<>(new OutputKey("client-2", "windowStartNotUsed", "windowStopNotUsed"), new Output(2, 2, 2));
 
-        List<KeyValue<OutputKey,Output>> actualResultAsList = outputTopic.readKeyValuesToList();
+        List<KeyValue<OutputKey, Output>> actualResultAsList = outputTopic.readKeyValuesToList();
 
-        final KeyValue<OutputKey,Output> actualResult = actualResultAsList.get(actualResultAsList.size()-1);
+        final KeyValue<OutputKey, Output> actualResult = actualResultAsList.get(actualResultAsList.size() - 1);
 
         assertEquals(expectedResult.value, actualResult.value);
         assertEquals(expectedResult.key.getName(), actualResult.key.getName());
-        assertTrue(actualResultAsList.size()==6);
+        assertTrue(actualResultAsList.size() == 6);
     }
 
     @Test
@@ -171,21 +170,21 @@ public class FizzBuzzAggregatorTest {
         inputTopic.pipeKeyValueList(inputValues);
 
         //Then
-        final List<KeyValue<OutputKey,Output>> expectedResults = Arrays.asList(
-                new KeyValue<>(new OutputKey("client-3", "", ""), new Output(2, 2, 2)),
-                new KeyValue<>(new OutputKey("client-4", "", ""), new Output(2, 2, 2))
+        final List<KeyValue<OutputKey, Output>> expectedResults = Arrays.asList(
+                new KeyValue<>(new OutputKey("client-3", "windowStartNotUsed", "windowStopNotUsed"), new Output(2, 2, 2)),
+                new KeyValue<>(new OutputKey("client-4", "windowStartNotUsed", "windowStopNotUsed"), new Output(2, 2, 2))
         );
 
-        List<KeyValue<OutputKey,Output>> actualResultAsList = outputTopic.readKeyValuesToList();
+        List<KeyValue<OutputKey, Output>> actualResultAsList = outputTopic.readKeyValuesToList();
 
-        final KeyValue<OutputKey,Output> actualResultFirstClient = actualResultAsList.get(actualResultAsList.size()-2);
-        final KeyValue<OutputKey,Output> actualResultSecondClient = actualResultAsList.get(actualResultAsList.size()-1);
+        final KeyValue<OutputKey, Output> actualResultFirstClient = actualResultAsList.get(actualResultAsList.size() - 2);
+        final KeyValue<OutputKey, Output> actualResultSecondClient = actualResultAsList.get(actualResultAsList.size() - 1);
 
         assertEquals(expectedResults.get(0).value, actualResultFirstClient.value);
         assertEquals(expectedResults.get(1).value, actualResultSecondClient.value);
         assertEquals(expectedResults.get(0).key.getName(), actualResultFirstClient.key.getName());
         assertEquals(expectedResults.get(1).key.getName(), actualResultSecondClient.key.getName());
-        assertTrue(actualResultAsList.size()==12);
+        assertTrue(actualResultAsList.size() == 12);
     }
 
 }
