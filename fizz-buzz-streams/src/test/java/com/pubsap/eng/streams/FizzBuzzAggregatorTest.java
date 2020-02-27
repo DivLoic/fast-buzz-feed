cc24d4d4496c81ef7dae5aa9979321e4b78d2bdd
DivLoic <ldivad@isep.fr>
2020-02-27 22:59:06 +0100

    simplify aggregator tests and refactor setup

--- fizz-buzz-streams/src/test/java/com/pubsap/eng/streams/FizzBuzzAggregatorTest.java
+++ fizz-buzz-streams/src/test/java/com/pubsap/eng/streams/FizzBuzzAggregatorTest.java
@@ -1,21 +1,26 @@
 package com.pubsap.eng.streams;
 
-import com.pubsap.eng.schema.*;
-
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
+import com.pubsap.eng.schema.InputKey;
+import com.pubsap.eng.schema.Item;
+import com.pubsap.eng.schema.ItemValue;
+import com.pubsap.eng.schema.Output;
 import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.*;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.apache.kafka.streams.kstream.WindowedSerdes.TimeWindowedSerde;
+import org.apache.kafka.streams.test.TestRecord;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import static com.pubsap.eng.common.FizzUtils.mapFromConfig;
 import static io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -24,23 +29,18 @@ public class FizzBuzzAggregatorTest {
 
     private static TopologyTestDriver testDriver;
     private static TestInputTopic<InputKey, Item> inputTopic;
-    private static TestOutputTopic<OutputKey, Output> outputTopic;
+    private static TestOutputTopic<Windowed<InputKey>, Output> outputTopic;
 
-    @BeforeAll
-    public static void setUp() {
-
-        final Config config = ConfigFactory.load();
-
+    @BeforeEach
+    public void setUp() {
         Properties properties = new Properties();
-        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, config.getString("bootstrap.servers"));
-        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, config.getString("application.id"));
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "unused:9092");
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "LOCAL-FBZZ-APP");
         properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
 
-        properties.putAll(mapFromConfig(config.getConfig("confluent-cloud-client")));
-
-        Map<String, Object> schemaRegistryConfigMap = mapFromConfig(config.getConfig("schema-registry-client"));
-        schemaRegistryConfigMap.put(SCHEMA_REGISTRY_URL_CONFIG, config.getString(SCHEMA_REGISTRY_URL_CONFIG));
+        Map<String, Object> schemaRegistryConfigMap =
+                Collections.singletonMap(SCHEMA_REGISTRY_URL_CONFIG, "http://unused:8081");
 
         String inputTopicName = "input-topic";
         String outputTopicName = "output-topic";
@@ -49,28 +49,31 @@ public class FizzBuzzAggregatorTest {
 
         SpecificAvroSerde<Item> itemSerde = new SpecificAvroSerde<>(mockedRegistry);
         SpecificAvroSerde<InputKey> inputKeySerde = new SpecificAvroSerde<>(mockedRegistry);
-        SpecificAvroSerde<OutputKey> outputKeySerde = new SpecificAvroSerde<>(mockedRegistry);
         SpecificAvroSerde<Output> outputSerde = new SpecificAvroSerde<>(mockedRegistry);
 
         itemSerde.configure(schemaRegistryConfigMap, false);
         outputSerde.configure(schemaRegistryConfigMap, false);
         inputKeySerde.configure(schemaRegistryConfigMap, true);
-        outputKeySerde.configure(schemaRegistryConfigMap, true);
-
 
         TimeWindows timeWindow = TimeWindows
 
-                .of(config.getDuration("window.size"))
+                .of(Duration.ofSeconds(20))
 
-                .advanceBy(config.getDuration("window.step"));
+                .advanceBy(Duration.ofSeconds(20));
+
+        SpecificAvroSerde<InputKey> innerSerde = new SpecificAvroSerde<>(mockedRegistry);
+        innerSerde.configure(schemaRegistryConfigMap, true);
+
+        TimeWindowedSerde<InputKey> outputKeySerde = new TimeWindowedSerde<>(innerSerde, timeWindow.size());
 
         Consumed<InputKey, Item> consumedItems = Consumed.with(inputKeySerde, itemSerde);
 
         Grouped<InputKey, Item> groupedItem = Grouped.with(inputKeySerde, itemSerde).withName("grouped-item");
 
-        Produced<OutputKey, Output> producedCounts = Produced.with(outputKeySerde, outputSerde);
+        Produced<Windowed<InputKey>, Output> producedCounts = Produced.with(outputKeySerde, outputSerde);
 
         StreamsBuilder builder = new StreamsBuilder();
+
         builder.stream(inputTopicName, consumedItems)
                 .groupByKey(groupedItem)
                 .windowedBy(timeWindow)
@@ -80,23 +83,23 @@ public class FizzBuzzAggregatorTest {
                         Materialized.with(inputKeySerde, outputSerde)
                 )
                 .toStream()
-                .map(FizzBuzzMapper.formatOutput)
                 .to(outputTopicName, producedCounts);
 
         testDriver = new TopologyTestDriver(builder.build(), properties);
 
-        inputTopic = testDriver.createInputTopic(inputTopicName, inputKeySerde.serializer(), itemSerde.serializer());
-        outputTopic = testDriver.createOutputTopic(outputTopicName, outputKeySerde.deserializer(), outputSerde.deserializer());
+        inputTopic = testDriver
+                .createInputTopic(inputTopicName, inputKeySerde.serializer(), itemSerde.serializer());
+        outputTopic = testDriver
+                .createOutputTopic(outputTopicName, outputKeySerde.deserializer(), outputSerde.deserializer());
     }
 
-    @AfterAll
-    public static void tearDown() {
+    @AfterEach
+    public void tearDown() {
         testDriver.close();
     }
 
     @Test
     public void aggregatorShouldGroupAllTypeOfItems() {
-
         //Given
         final List<KeyValue<InputKey, Item>> inputValues = Arrays.asList(
                 new KeyValue<>(new InputKey("client-1"), new Item(ItemValue.Fizz)),
@@ -108,83 +111,85 @@ public class FizzBuzzAggregatorTest {
         inputTopic.pipeKeyValueList(inputValues);
 
         //Then
-        final KeyValue<OutputKey, Output> expectedResult = new KeyValue<>(new OutputKey("client-1", "windowStartNotUsed", "windowStopNotUsed"), new Output(1, 1, 1));
+        Output result1 = outputTopic.readValue();
+        Output result2 = outputTopic.readValue();
+        Output result3 = outputTopic.readValue();
 
-        List<KeyValue<OutputKey, Output>> result = outputTopic.readKeyValuesToList();
+        assertEquals(Output.newBuilder().setFizz(1).build(), result1);
+        assertEquals(Output.newBuilder().setFizz(1).setBuzz(1).build(), result2);
+        assertEquals(Output.newBuilder().setFizz(1).setBuzz(1).setFizzBuzz(1).build(), result3);
 
-        final KeyValue<OutputKey, Output> actualResult = result.get(result.size() - 1);
-
-        assertEquals(expectedResult.value, actualResult.value);
-        assertEquals(expectedResult.key.getName(), actualResult.key.getName());
-        assertTrue(result.size() == 3);
-    }
-
-    @Test
-    public void aggregatorShouldGroupItemBasedOnItemValue() {
-
-        //Given
-        final List<KeyValue<InputKey, Item>> inputValues = Arrays.asList(
-                new KeyValue<>(new InputKey("client-2"), new Item(ItemValue.Fizz)),
-                new KeyValue<>(new InputKey("client-2"), new Item(ItemValue.Fizz)),
-                new KeyValue<>(new InputKey("client-2"), new Item(ItemValue.Buzz)),
-                new KeyValue<>(new InputKey("client-2"), new Item(ItemValue.Buzz)),
-                new KeyValue<>(new InputKey("client-2"), new Item(ItemValue.FizzBuzz)),
-                new KeyValue<>(new InputKey("client-2"), new Item(ItemValue.FizzBuzz))
-        );
-
-        //When
-        inputTopic.pipeKeyValueList(inputValues);
-
-        //Then
-        final KeyValue<OutputKey, Output> expectedResult = new KeyValue<>(new OutputKey("client-2", "windowStartNotUsed", "windowStopNotUsed"), new Output(2, 2, 2));
-
-        List<KeyValue<OutputKey, Output>> actualResultAsList = outputTopic.readKeyValuesToList();
-
-        final KeyValue<OutputKey, Output> actualResult = actualResultAsList.get(actualResultAsList.size() - 1);
-
-        assertEquals(expectedResult.value, actualResult.value);
-        assertEquals(expectedResult.key.getName(), actualResult.key.getName());
-        assertTrue(actualResultAsList.size() == 6);
+        assertTrue(outputTopic.isEmpty());
     }
 
     @Test
     public void aggregatorShouldGroupItemBasedOnItemValueForeachKey() {
-
         //Given
         final List<KeyValue<InputKey, Item>> inputValues = Arrays.asList(
+                new KeyValue<>(new InputKey("client-1"), new Item(ItemValue.Fizz)),
+                new KeyValue<>(new InputKey("client-2"), new Item(ItemValue.Fizz)),
+                new KeyValue<>(new InputKey("client-2"), new Item(ItemValue.Fizz)),
                 new KeyValue<>(new InputKey("client-3"), new Item(ItemValue.Fizz)),
-                new KeyValue<>(new InputKey("client-4"), new Item(ItemValue.Fizz)),
-                new KeyValue<>(new InputKey("client-3"), new Item(ItemValue.Fizz)),
-                new KeyValue<>(new InputKey("client-4"), new Item(ItemValue.Fizz)),
                 new KeyValue<>(new InputKey("client-3"), new Item(ItemValue.Buzz)),
-                new KeyValue<>(new InputKey("client-4"), new Item(ItemValue.Buzz)),
                 new KeyValue<>(new InputKey("client-3"), new Item(ItemValue.Buzz)),
-                new KeyValue<>(new InputKey("client-4"), new Item(ItemValue.Buzz)),
+                new KeyValue<>(new InputKey("client-1"), new Item(ItemValue.Buzz)),
+                new KeyValue<>(new InputKey("client-2"), new Item(ItemValue.Buzz)),
                 new KeyValue<>(new InputKey("client-3"), new Item(ItemValue.FizzBuzz)),
-                new KeyValue<>(new InputKey("client-4"), new Item(ItemValue.FizzBuzz)),
-                new KeyValue<>(new InputKey("client-3"), new Item(ItemValue.FizzBuzz)),
-                new KeyValue<>(new InputKey("client-4"), new Item(ItemValue.FizzBuzz))
+                new KeyValue<>(new InputKey("client-1"), new Item(ItemValue.FizzBuzz)),
+                new KeyValue<>(new InputKey("client-2"), new Item(ItemValue.FizzBuzz)),
+                new KeyValue<>(new InputKey("client-1"), new Item(ItemValue.FizzBuzz))
         );
 
         //When
         inputTopic.pipeKeyValueList(inputValues);
 
         //Then
-        final List<KeyValue<OutputKey, Output>> expectedResults = Arrays.asList(
-                new KeyValue<>(new OutputKey("client-3", "windowStartNotUsed", "windowStopNotUsed"), new Output(2, 2, 2)),
-                new KeyValue<>(new OutputKey("client-4", "windowStartNotUsed", "windowStopNotUsed"), new Output(2, 2, 2))
-        );
+        Map<String, Output> resultIt = outputTopic
+                .readKeyValuesToMap()
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        record -> record.getKey().key().getName(),
+                        Map.Entry::getValue
+                ));
 
-        List<KeyValue<OutputKey, Output>> actualResultAsList = outputTopic.readKeyValuesToList();
+        Output result1 = resultIt.remove("client-1");
+        Output result2 = resultIt.remove("client-2");
+        Output result3 = resultIt.remove("client-3");
 
-        final KeyValue<OutputKey, Output> actualResultFirstClient = actualResultAsList.get(actualResultAsList.size() - 2);
-        final KeyValue<OutputKey, Output> actualResultSecondClient = actualResultAsList.get(actualResultAsList.size() - 1);
+        assertEquals(Output.newBuilder().setFizz(1).setBuzz(1).setFizzBuzz(2).build(), result1);
+        assertEquals(Output.newBuilder().setFizz(2).setBuzz(1).setFizzBuzz(1).build(), result2);
+        assertEquals(Output.newBuilder().setFizz(1).setBuzz(2).setFizzBuzz(1).build(), result3);
 
-        assertEquals(expectedResults.get(0).value, actualResultFirstClient.value);
-        assertEquals(expectedResults.get(1).value, actualResultSecondClient.value);
-        assertEquals(expectedResults.get(0).key.getName(), actualResultFirstClient.key.getName());
-        assertEquals(expectedResults.get(1).key.getName(), actualResultSecondClient.key.getName());
-        assertTrue(actualResultAsList.size() == 12);
+        assertTrue(resultIt.isEmpty());
+        assertTrue(outputTopic.isEmpty());
     }
 
+    @Test
+    public void aggregatorShouldGroupItemOverATimeWindow() {
+        //Given
+        final List<TestRecord<InputKey, Item>> inputValues = Stream.of(
+                Instant.parse("2019-06-24T18:10:00Z"),
+                Instant.parse("2019-06-24T18:10:05Z"),
+                Instant.parse("2019-06-24T18:10:15Z"),
+                Instant.parse("2019-06-24T18:10:30Z"),
+                Instant.parse("2019-06-24T18:10:40Z"),
+                Instant.parse("2019-06-24T18:11:55Z")
+        )
+                .map((instant) -> new TestRecord<>(new InputKey("client"), new Item(ItemValue.FizzBuzz), instant))
+                .collect(Collectors.toList());
+
+        //When
+        inputTopic.pipeRecordList(inputValues);
+
+        //Then
+        assertEquals(outputTopic.readRecord().getValue(), Output.newBuilder().setFizzBuzz(1).build());
+        assertEquals(outputTopic.readRecord().getValue(), Output.newBuilder().setFizzBuzz(2).build());
+        assertEquals(outputTopic.readRecord().getValue(), Output.newBuilder().setFizzBuzz(3).build());
+        assertEquals(outputTopic.readRecord().getValue(), Output.newBuilder().setFizzBuzz(1).build());
+        assertEquals(outputTopic.readRecord().getValue(), Output.newBuilder().setFizzBuzz(1).build());
+        assertEquals(outputTopic.readRecord().getValue(), Output.newBuilder().setFizzBuzz(1).build());
+
+        assertTrue(outputTopic.isEmpty());
+    }
 }
